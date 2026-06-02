f0ed8cfd426485327303412b43fd593462fd3069
Lucas Malinski <mailpatriota@gmail.com>
2026-06-02 12:08:48 -0300

    não baixar se já existe

--- ingestion/main.py
+++ ingestion/main.py
@@ -8,16 +8,24 @@ def main():
     raw_dir.mkdir(parents=True, exist_ok=True)
 
     for ano in range(2022, 2027):
-        url = f"https://api3.saude.df.gov.br/dados_csv/?ano={ano}&mes=disable&complexidade=disable&parto=disable&cirurgia=disable&obito=disable"
         output_file = raw_dir / f"dados_{ano}.csv"
+
+        # Checa existência do arquivo antes de baixar
+        if output_file.exists():
+            print(f"Arquivo para {ano} já existe. Pulando...")
+            continue
+
+        url = f"https://api3.saude.df.gov.br/dados_csv/?ano={ano}&mes=disable&complexidade=disable&parto=disable&cirurgia=disable&obito=disable"
         print(f"Downloading {ano}...")
 
         try:
             response = requests.get(url)
             response.raise_for_status()
-            with open(output_file, "wb") as file:
-                file.write(response.content)
+
+            # Using pathlib's write_bytes for a cleaner look
+            output_file.write_bytes(response.content)
             print(f"Baixado {ano} -> {output_file}")
+
         except requests.RequestException as error:
             print(f"Falha ao baixar {ano}: {error}")
 
